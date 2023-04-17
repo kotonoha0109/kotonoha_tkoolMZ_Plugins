653ef43b880be461572654946ff0be603e758adf
kotonoha* <128649205+kotonoha0109@users.noreply.github.com>
2023-04-17 18:44:11 +0900

    v1.0 Stream版を統合

--- plugins/php/request.php
+++ plugins/php/request.php
@@ -10,7 +10,7 @@
 	//
 	// --------------------------------------------------------------------------
 
-	// ここにAPIキーを設定する
+	// APIキーを設定
 	$api_key = 'sk-*****************************';
 
 	// ------------------------------------------------------------------------------
@@ -23,41 +23,41 @@
 	// OpenAI APIにリクエストを送信
 	$ch = curl_init('https://api.openai.com/v1/chat/completions');
 	curl_setopt_array($ch, [
-	    CURLOPT_POST => true,
-	    CURLOPT_RETURNTRANSFER => true,
-	    CURLOPT_SSL_VERIFYPEER => false,
-	    CURLOPT_SSL_VERIFYHOST => false,
-	    CURLOPT_HTTPHEADER => [
-	        'Content-Type: application/json',
-	        'Authorization: Bearer ' . $api_key,
-	    ],
-	    CURLOPT_POSTFIELDS => json_encode($post_data),
+		CURLOPT_POST => true,
+		CURLOPT_RETURNTRANSFER => false,
+		CURLOPT_HTTPHEADER => [
+			'Content-Type: application/json',
+			'Authorization: Bearer ' . $api_key,
+		],
+		CURLOPT_POSTFIELDS => json_encode($post_data),
+		CURLOPT_WRITEFUNCTION => function ($ch, $chunk) {
+			echo $chunk;
+			ob_flush();
+			flush();
+			return strlen($chunk);
+		},
 	]);
 
-	// APIからのレスポンスを取得
+	// レスポンスを取得
 	$response = curl_exec($ch);
-	$curl_error = curl_error($ch);
+    
+	// エラーがある場合
+    $curl_error = curl_errno($ch);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 	if ($curl_error) {
 	    // cURLエラー
 	    echo json_encode(array("error" => array("code" => $curl_error)));
 	} else {
-	    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	    if ($http_status >= 400) {
-	        // ChatGPT APIエラー
-	        $errorText = $response;
-	        $errorJson = json_decode($errorText, true);
-	        http_response_code($http_status);
-	        // メッセージを出力するとAPIキーがRMMZで出力される恐れがあるので
-	        // コードの方を出力すること
-	        $errorMessage = $errorJson["error"]["code"];
-	        $responseJson = json_encode(array("error" => array("code" => $errorMessage)));
-	        echo $responseJson;
-	    } else {
-	        // 正常なレスポンスの出力
-	        http_response_code($http_status);
-	        echo $response;
-	    }
+		// ChatGPT APIエラー
+	    $errorText = $response;
+	    $errorJson = json_decode($errorText, true);
+	    http_response_code($http_status);
+	    // メッセージを出力するとAPIキーがRMMZで出力される恐れがあるので
+	    // コードの方を出力すること
+	    $errorMessage = $errorJson["error"]["code"];
+	    $responseJson = json_encode(array("error" => array("code" => $errorMessage)));
+	    echo $responseJson;
 	}
 
 	curl_close($ch);
